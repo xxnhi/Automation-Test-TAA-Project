c064e90c82d7945af88406c9d7b1d2feb6411650
Xuan Nhi <xuannhi27.work@gmail.com>
2024-03-01 21:35:12 +0700

    add AccountPage and AccountTest

--- src/test/java/anhtester/pages/AccountPage.java
+++ src/test/java/anhtester/pages/AccountPage.java
@@ -1,6 +1,7 @@
 package anhtester.pages;
 
 import anhtester.common.ValidateHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -9,12 +10,31 @@ public class AccountPage {
     private ValidateHelper validateHelper;
     private WebDriverWait wait;
 
+    private By changePassBtn = By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/main[1]/section[1]/article[3]/button[1]/span[1]");
+
+    private By oldPassInput = By.xpath("/html[1]/body[1]/div[1]/div[9]/div[1]/form[1]/div[1]/div[1]/div[1]/span[1]/input[1]");
+
+    private By newPassInput = By.xpath("/html[1]/body[1]/div[1]/div[9]/div[1]/form[1]/div[1]/div[2]/div[1]/span[1]/input[1]");
+    private By confirmPassInput = By.xpath("/html[1]/body[1]/div[1]/div[9]/div[1]/form[1]/div[1]/div[3]/div[1]/span[1]/input[1]");
+    private By confirmBtn = By.xpath("/html[1]/body[1]/div[1]/div[9]/div[1]/form[1]/div[2]/button[1]");
+
+
     public AccountPage(WebDriver driver){
         this.driver = driver;
         validateHelper = new ValidateHelper(driver);
     }
 
-    public void ChangePassword(){
-        
+    public void goToChangePasswordPopup(){
+        validateHelper.scrollToElement(changePassBtn);
+        validateHelper.clickElement(changePassBtn);
+    }
+
+    public void ChangePassword(String oldPass, String newPass, String confirmPass){
+        validateHelper.waitForPageLoaded();
+        validateHelper.setText(oldPassInput, oldPass);
+        validateHelper.setText(newPassInput, newPass);
+        validateHelper.setText(confirmPassInput, confirmPass);
+        validateHelper.clickElement(confirmBtn);
+
     }
 }
--- src/test/java/anhtester/testcase/AccountTest.java
+++ src/test/java/anhtester/testcase/AccountTest.java
@@ -1,4 +1,44 @@
 package anhtester.testcase;
 
+import anhtester.common.ValidateHelper;
+import anhtester.pages.AccountPage;
+import anhtester.pages.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class AccountTest {
+    private WebDriver driver;
+    private ValidateHelper validateHelper;
+    private AccountPage accountPage;
+    private HomePage homePage;
+    @BeforeClass
+    public void setupBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //==========quan trong============
+        validateHelper = new ValidateHelper(driver);
+        //==========quan trong============
+        accountPage = new AccountPage(driver);
+        homePage = new HomePage(driver);
+    }
+
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test(priority = 1)
+    public void ChangePassword() throws Exception {
+        homePage.goToAccountPage();
+        accountPage.ChangePassword("","","");
+
+    }
 }
